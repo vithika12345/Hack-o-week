a944380f33946bb536e386aa522d1aa3c4fa58dc
vithika12345 <vithikasaoji@gmail.com>
2026-07-15 10:42:46 +0530

    Add files via upload

---
+++ Quotes API/quotes.py
@@ -0,0 +1,10 @@
+
+QUOTES = [
+    {"id": 1, "author": "Albert Einstein", "text": "Imagination is more important than knowledge."},
+    {"id": 2, "author": "Maya Angelou", "text": "If you don't like something, change it. If you can't change it, change your attitude."},
+    {"id": 3, "author": "Steve Jobs", "text": "Your time is limited, so don't waste it living someone else's life."},
+    {"id": 4, "author": "Nelson Mandela", "text": "It always seems impossible until it's done."},
+    {"id": 5, "author": "Eleanor Roosevelt", "text": "The future belongs to those who believe in the beauty of their dreams."},
+    {"id": 6, "author": "Confucius", "text": "It does not matter how slowly you go as long as you do not stop."},
+    {"id": 7, "author": "Walt Disney", "text": "The way to get started is to quit talking and begin doing."},
+]
